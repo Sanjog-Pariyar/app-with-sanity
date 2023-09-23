e4c0d0b8e17f112f025dd3796256dc9a03eb058d
Sanjog Pariyar <sanjogpariyar17@gmail.com>
2023-09-23 19:14:47 +0545

    second message

---
+++ main-app/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center border-2 border-green-600 w-3/4">
+      <h1 className='text-red-600 text-2xl font-bold border-2 border-indigo-600 bg-slate-400 w-1/2 p-2'>Hello Next</h1>
+      <h2>I am Sanjog</h2>
+    </div>
+  )
+}
---
+++ sanityIO/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wsw4lsd6',
+    dataset: 'production'
+  }
+})
